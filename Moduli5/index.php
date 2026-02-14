adf1cacc2b5a682e7a623a65a30d7d143b5ce57b
ArianitaGashi <arianita.gashi@digitalschool.tech>
2026-02-14 14:54:16 +0100

    mod 6 7

--- Moduli5/index.php
+++ Moduli5/index.php
@@ -17,16 +17,23 @@
 <?php
 
     $dogs = [
-        array("Husky", "Siberia", 15),
-        array("Bulldog", "England", 10),
-        array("Chihuahua", "Mexico", 20)
+
+      array("Husky", "Siberia", 15),
+       array("Bulldog", "England", 10), 
+        array("Chihuahua", "Mexico", 20) 
     ];
 
     echo $dogs[0][0]. " Origin: ".  $dogs[0][1]. " Life Span: ". $dogs[0][2]. "<hr>";
     echo $dogs[1][0]. " Origin: ".  $dogs[1][1]. " Life Span: ". $dogs[1][2]. "<hr>";
     echo $dogs[2][0]. " Origin: ".  $dogs[2][1]. " Life Span: ". $dogs[2][2]. "<hr>";
 
-
+    for($x = 0; $x<3; $x++){
+        echo "<ul>";
+        for($y = 0; $y<3; $y++){
+            echo "<li>".$dogs[$x][$y]."</li>";
+        }
+        echo "</ul>";
+    }
 
     $phones = [
         ["Iphone 15", 20, 15],
@@ -42,5 +49,19 @@
         echo "</tr>";
     }
     echo "</table>";
+
+
+
+    $grades = [
+        "Matematike" => "3",
+        "Biologji" => "2",
+        "Histori" => "5",
+        "Muzik" => "4"
+    ];
+
+    echo "Math grade is ".$grades["Matematike"]."<br>";
+    echo "Bio grade is ".$grades["Biologji"]."<br>";
+    echo "History grade is ".$grades["Histori"]."<br>";
+    echo "Music grade is ".$grades["Muzik"]."<br>";
 
 ?>
